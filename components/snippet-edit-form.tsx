db0df2d6e299e03512c1d6db897a90ca6620c70f
sangwook.yoo <sangwook.yoo@cj.net>
2024-02-14 12:58:14 +0900

    chore: Add prettier config

--- components/snippet-edit-form.tsx
+++ components/snippet-edit-form.tsx
@@ -1,13 +1,21 @@
-"use client";
+'use client'
 
-import { Editor } from "@monaco-editor/react";
-import { Snippet } from "@prisma/client";
+import { Editor } from '@monaco-editor/react'
+import { Snippet } from '@prisma/client'
+import { useState } from 'react'
 
 interface SnippetEditFormProps {
-  snippet: Snippet;
+  snippet: Snippet
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+  const [code, setCode] = useState(snippet.code)
+
+  function handleEditorChange(value: string = ''): void {
+    // console.log("value", value);
+    setCode(value)
+  }
+
   return (
     <>
       {/* <div>Client compoennt has snippet with title: {snippet.title}</div> */}
@@ -15,9 +23,10 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         theme="vs-dark"
         height="30vh"
         defaultLanguage="javascript"
-        defaultValue={snippet.code}
+        defaultValue={code}
         options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
       />
     </>
-  );
+  )
 }
